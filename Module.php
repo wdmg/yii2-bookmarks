528a3e9d50d6528ba04a06a49a8734cfaddf6fe9
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2021-01-16 02:24:56 +0200

    Fixed UrlManager init, extended module dashboardNavItems() method

--- Module.php
+++ Module.php
@@ -72,7 +72,7 @@ class Module extends BaseModule
     /**
      * {@inheritdoc}
      */
-    public function dashboardNavItems($createLink = false)
+    public function dashboardNavItems($options = false)
     {
         $items = [
             'label' => $this->name,
